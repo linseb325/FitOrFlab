ba4ae82ae77f04ed3beaa66db1b957c9dff50a3e
linseb325 <linseb325@gmail.com>
2017-12-31 11:04:08 -0600

    Exercise title, background color, and image now change on the detail screen based on which exercise is selected. Completed the tutorials for this section.

--- app/src/main/java/com/example/linseb325/fitorflab/DetailActivity.java
+++ app/src/main/java/com/example/linseb325/fitorflab/DetailActivity.java
@@ -1,13 +1,56 @@
 package com.example.linseb325.fitorflab;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    // Fields for outlets
+    TextView exerciseText;
+    ImageView exerciseImage;
+    LinearLayout mainBG;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        // Setting outlets
+        this.exerciseText = (TextView) findViewById(R.id.exerciseTitleTextView);
+        this.exerciseImage = (ImageView) findViewById(R.id.imageView);
+        this.mainBG = (LinearLayout) findViewById(R.id.mainBackgroundLayout);
+
+        // Set the title text.
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        this.exerciseText.setText(exerciseTitle);
+
+        // Set the color and image based on which exercise was selected.
+        if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS)) {
+            // Chose weight lifting. Update UI.
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.weight, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        } else if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)) {
+            // Chose yoga. Update UI.
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.lotus, getApplication().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+        } else {
+            // Chose cardio. Update UI.
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.heart, getApplication().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+        }
+
+
+
+
     }
+
+
+
+
+
 }
--- app/src/main/java/com/example/linseb325/fitorflab/MainActivity.java
+++ app/src/main/java/com/example/linseb325/fitorflab/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.linseb325.fitorflab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,19 +8,28 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // Strings to pass as extras
     public static final String EXERCISE_WEIGHTS = "Weight Lifting";
     public static final String EXERCISE_YOGA = "Yoga";
     public static final String EXERCISE_CARDIO = "Cardio";
+
+    // A key name to look up later
+    public static final String EXTRA_ITEM_TITLE = "extra.item.title";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // Button outlets
         RelativeLayout weightButton = (RelativeLayout) findViewById(R.id.weightLiftingButton);
         RelativeLayout yogaButton = (RelativeLayout) findViewById(R.id.yogaButton);
         RelativeLayout cardioButton = (RelativeLayout) findViewById(R.id.cardioButton);
 
+
+        // What happens when we tap each button?
         weightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,11 +63,12 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    private void loadDetailActivity(String exerciseTitle) {
-        System.out.println(exerciseTitle);
+    private void loadDetailActivity(String selectedExerciseTitle) {
+        System.out.println("Selected " + selectedExerciseTitle);
 
-
-
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        intent.putExtra(MainActivity.EXTRA_ITEM_TITLE, selectedExerciseTitle);
+        startActivity(intent);
     }
 
 
